d238173e79c8be806a6af3d989fda7fc0a66ac37
ortizfram <ortizfranco48@gmail.com>
2023-07-21 08:32:14 -0300

    hanfle signup errors

--- tasks/views.py
+++ tasks/views.py
@@ -25,6 +25,12 @@ def signup(request):
                 user.save()
                 return HttpResponse("User created successfully!")
             except:
-                return HttpResponse("Username already exists")
+                return render(
+                    request, "signup.html", {"error": "Username already exists"}
+                )
         # send error message: httpResponse
-        return HttpResponse("Password do not match")
+        return render(
+            request,
+            "signup.html",
+            {"form": UserCreationForm, "error": "Password do not match"},
+        )
