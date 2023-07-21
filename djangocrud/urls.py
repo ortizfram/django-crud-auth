7f242d3619fd364381d6d60737a253b923053dcc
ortizfram <ortizfranco48@gmail.com>
2023-07-21 09:35:10 -0300

    signup logout login and redirect

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
     path("signup/", views.signup, name="signup"),
+    path("logout/", views.signout, name="logout"),
     path("tasks/", views.tasks, name="tasks"),
 ]
