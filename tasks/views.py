2daef37991a1e761ae5f2fe6362dfdbda3241327
ortizfram <ortizfranco48@gmail.com>
2023-07-27 12:04:58 -0300

    update task

--- tasks/views.py
+++ tasks/views.py
@@ -51,8 +51,21 @@ def tasks(request):
     })
 
 def task_detail(request, task_id):
-    task = get_object_or_404(Task, pk=task_id)
-    return render(request, 'task_detail.html', {'task':task})
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {'task':task, 'form':form})
+    else:
+        # update and save
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        # error
+        except ValueError:
+            return render(request, 'task_detail.html', {'task':task, 'form':form, 'error':"Error updating tasks"})
+
 
 def create_task(request):
     # return creation form
